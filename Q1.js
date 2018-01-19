654058b2f5fe9dd2070cc3fc16ea9f82cc8c112b
unknown <qokberk_akdeniz@hotmail.com>
2018-01-19 20:03:49 +0300

    fix

--- Q1.js
+++
@@ -1,4 +0,0 @@
-nn = Array.from(new Array(1000-1), (val, index) => index+1);
-nn_filtered = nn.filter((x) => {if (x % 3 == 0 || x % 5 == 0) return x})
-sum = nn_filtered.reduce((a, b) => {return a+b}, 0)
-console.log(sum)
--- Q6.js
+++
@@ -1,4 +0,0 @@
-let squares, squares_of_sum, n = 100;
-squares = Array.from(new Array(n+1), (val, index) => index ** 2).reduce((a, b) => a+b, 0)
-squares_of_sum = Array.from(new Array(n+1), (val, index) => index).reduce((a, b) => a+b, 0) ** 2
-console.log(squares_of_sum - squares)
